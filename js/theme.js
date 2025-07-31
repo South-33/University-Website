69830228baa1b2350d17959598f9ef7088ef3231
South33 <126777514+nyxy33@users.noreply.github.com>
2025-07-31 15:34:58 +0700

    Updated content

--- js/theme.js
+++ js/theme.js
@@ -3,7 +3,7 @@ tailwind.config = {
     extend: {
       colors: {
         'dark-blue': '#0A0A3E',
-        'light-lavender': '#E9E8F5',
+        'light-lavender': '#f3f2f7',
       },
       fontFamily: {
         heading: ['Lora', 'serif'],
